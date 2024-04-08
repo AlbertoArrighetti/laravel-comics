6405e7025bc993d4677f70c320bd2679547fe150
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-08 17:01:01 +0200

    nav bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts/app')
 
 @section('content')
-    CONTENT GOES HERE
+
 @endsection
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,35 @@
+<nav>
+    <div id="top-bar">
+        <div class="container">
+            <div>Dc power visa®</div>
+            <div>additional dc sites ▼</div>
+        </div>
+    </div>
+    <div id="nav">
+        <div class="container">
+            <div>
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo dc">
+            </div>
+            <div class="list">
+                <ul class="mb-0">
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li>
+                        <a href="#">shop</a>
+                        <i>▼</i>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                searchbar
+            </div>
+        </div>
+    </div>
+</nav>
