4e59d8c8969d3e768ced9a1f6eb812286b92a883
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-08 17:42:58 +0200

    main section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts/app')
 
 @section('content')
+<main id="homepage">
+    <div class="container">
+        <div class="my_btn absolute">
+            <button>current series</button>
+        </div>
 
+        <div class="row row-cols-6">
+            
+            @foreach ($comics as $currentComic)
+            <div class="comic-card">
+                <div class="thumb">
+                    <img src="{{$currentComic['thumb']}}" alt="">
+                </div>
+    
+                <div class="title">
+                    <div>{{$currentComic['series']}}</div>
+                </div>
+            </div>
+            @endforeach
+        </div> 
+
+        <div id="bottom-main" class="d-flex justify-content-center">
+            <div class="my_btn more">
+                <button>load more</button>
+            </div>
+        </div>
+    </div>
+</main>
 @endsection
